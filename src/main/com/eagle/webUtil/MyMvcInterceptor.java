6f6d952c0dece6e14403929d6cbbf2d38c00fa7e
jiangzhifei <jiangzhifei@eskygo.com>
2015-08-07 16:01:00 +0800

    超时检查

--- src/main/com/eagle/webUtil/MyMvcInterceptor.java
+++ src/main/com/eagle/webUtil/MyMvcInterceptor.java
@@ -3,14 +3,12 @@ package com.eagle.webUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.context.request.WebRequestInterceptor;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by jiang on 15/7/30.
@@ -20,23 +18,36 @@ public class MyMvcInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        logger.info("Enter MyMvcInterceptor");
-        Object user = request.getSession().getAttribute("user");
+        logger.info("Enter MyMvcInterceptor.preHandle,request=[{}]", request.getRequestURL());
+
+        HttpSession session = request.getSession();
+        if (null == session) {
+            response.sendRedirect("/index.jsp");
+            response.setStatus(HttpStatus.NOT_MODIFIED.value());
+
+            logger.info("Exit MyMvcInterceptor.preHandle");
+            return false;
+        }
+        Object user = session.getAttribute("user");
         if (null == user) {
             response.sendRedirect("/index.jsp");
             response.setStatus(HttpStatus.NOT_MODIFIED.value());
+
+            logger.info("Exit MyMvcInterceptor.preHandle");
             return false;
         }
+
+        logger.info("Exit MyMvcInterceptor.preHandle");
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        logger.info("Enter MyMvcInterceptor");
+//        logger.info("Enter MyMvcInterceptor.postHandle");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        logger.info("Enter MyMvcInterceptor");
+//        logger.info("Enter MyMvcInterceptor.afterCompletion");
     }
 }
